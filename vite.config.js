f9efa2b2fca82f31c139e288bead08f291aa8322
Thewolf8 <140300286+Thewolf8@users.noreply.github.com>
2026-05-05 20:45:50 +0000

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,11 +1,18 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+const { defineConfig } = require('vite')
+const react = require('@vitejs/plugin-react')
 
-// https://vite.dev/config/
-export default defineConfig({
-  plugins: [react()],
-
-export default defineConfig({
+module.exports = defineConfig({
   plugins: [react()],
   base: '/cristina/',
+  optimizeDeps: {
+    exclude: [
+      '@react-three/fiber',
+      '@react-three/drei',
+      '@react-three/postprocessing',
+    ],
+  },
+  build: {
+    target: 'esnext',
+    chunkSizeWarningLimit: 3000,
+  },
 })
